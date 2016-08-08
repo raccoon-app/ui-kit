d5a6c25870f8c86c3206da50e1d21c52edbd65cc
Andrii Mokhovyk <andrey.mokhovyk@gmail.com>
2016-08-08 11:13:27 +0300

    Added content

--- src/js/actions/projectSelection.js
+++ src/js/actions/projectSelection.js
@@ -13,7 +13,19 @@ const projectList = [
         url: 'http://mokhovyk.com/raccoon/',
         title: 'raccoon design',
         _id: 'rccn',
+        image: 'http://mokhovyk.com/raccoon/076FEA6F-2118-4F87-AD9F-2043812E9DF9/artboard.png',
+    },
+    {
+        url: 'http://design.oweather.net/',
+        title: 'weather 2',
+        _id: 'wthr2',
         image: 'http://design.oweather.net/6D6CF3F4-6064-4B05-AFBB-EBFC79333CC2/artboard.png',
+    },
+    {
+        url: 'http://mokhovyk.com/raccoon/',
+        title: 'raccoon design 2',
+        _id: 'rccn2',
+        image: 'http://mokhovyk.com/raccoon/076FEA6F-2118-4F87-AD9F-2043812E9DF9/artboard.png',
     },
 ];
 
