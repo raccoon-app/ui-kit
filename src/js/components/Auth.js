e1193248268270e6f518c8441fcfc2562952620e
Viktoriia_Goncharuk <vitokg@gmail.com>
2016-08-08 16:37:30 +0300

    Login form page: added forgot pwd block; removed <br />,  element position fixes

--- src/js/components/Auth.js
+++ src/js/components/Auth.js
@@ -6,22 +6,28 @@ import EpamLogoSvg from '../../img/e-p-a-m-logo-white.svg';
 const LoginScreen = ({ formLogin, epamLogin }) => (
     <section className="app auth">
         <header className="auth__header">
-            <img className="auth__raccoon-logo"
-                src={RaccoonLogoSvg}
-            />
-            <br />
-            <img className="auth__epam-logo"
-                src={EpamLogoSvg}
-            />
+            <figure className="auth__figure">
+                <img className="auth__raccoon-logo"
+                    src={RaccoonLogoSvg} alt="Raccoon App Logo"
+                />
+            </figure>
+            <figure className="auth__figure">
+                <img className="auth__epam-logo"
+                    src={EpamLogoSvg} alt="EPAM Logo"
+                />
+            </figure>
             <h1 className="auth__title">RACCOON APP</h1>
             <p>Sign in with your organizational account</p>
         </header>
         <form className="auth-form">
             <Input type="text" name="authLogin" label="LOGIN" />
             <Input type="password" name="authPassword" label="PASSWORD" />
-            <button onClick={formLogin} className="auth-form__btn btn btn_green">SignIn</button>
-            <button onClick={epamLogin} className="auth-form__btn auth-form__btn_right btn">EPAM SignIn</button>
+            <button onClick={formLogin} className="auth-form__btn btn btn_green">EPAM SignIn</button>
+            <button onClick={epamLogin} className="auth-form__btn auth-form__btn_right btn">SignIn</button>
         </form>
+        <div className="forgot-pwd">
+            <p>Forgot your password? <br /> Reset on <a className="forgot-pwd__link" href="//password.epam.com">password.epam.com</a></p>
+        </div>
     </section>
 );
 
