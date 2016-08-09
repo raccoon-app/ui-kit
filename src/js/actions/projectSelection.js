55746ae97591904466bbfb6fcc23aff32f717958
Andrii Mokhovyk <andrey.mokhovyk@gmail.com>
2016-08-09 12:59:43 +0300

    Added test project

--- src/js/actions/projectSelection.js
+++ src/js/actions/projectSelection.js
@@ -16,16 +16,10 @@ const projectList = [
         image: 'http://mokhovyk.com/raccoon/076FEA6F-2118-4F87-AD9F-2043812E9DF9/artboard.png',
     },
     {
-        url: 'http://design.oweather.net/',
-        title: 'weather 2',
-        _id: 'wthr2',
-        image: 'http://design.oweather.net/6D6CF3F4-6064-4B05-AFBB-EBFC79333CC2/artboard.png',
-    },
-    {
-        url: 'http://mokhovyk.com/raccoon/',
-        title: 'raccoon design 2',
-        _id: 'rccn2',
-        image: 'http://mokhovyk.com/raccoon/076FEA6F-2118-4F87-AD9F-2043812E9DF9/artboard.png',
+        url: 'http://mokhovyk.com/raccoon_test/',
+        title: 'test design',
+        _id: 'tst',
+        image: 'http://mokhovyk.com/raccoon_test/47191062-5006-483D-B616-0503EC65923D/artboard.png',
     },
 ];
 
