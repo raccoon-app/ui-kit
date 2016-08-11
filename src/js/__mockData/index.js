4b52813917e50e0731e1684afc86cc1ea006b762
Andrii Mokhovyk <andrey.mokhovyk@gmail.com>
2016-08-11 16:59:01 +0300

    Created mock response

---
+++ src/js/__mockData/index.js
@@ -0,0 +1,20 @@
+export const projectList = [
+    {
+        _id: 'rccn',
+        url: 'http://design.oweather.net/Raccoon/',
+        title: 'Raccoon',
+        image: 'http://mokhovyk.com/raccoon_content/thumbnails/raccoon.png'
+    },
+    {
+        _id: 'tstp',
+        url: 'http://design.oweather.net/RaccoonTestProject/',
+        title: 'Test Project',
+        image: 'http://mokhovyk.com/raccoon_content/thumbnails/test-project.png'
+    },
+    {
+        _id: 'wthr',
+        url: 'http://design.oweather.net/oWeather/',
+        title: 'oWeather',
+        image: 'http://mokhovyk.com/raccoon_content/thumbnails/oweather.png'
+    },
+];
--- src/js/actions/projectSelection.js
+++ src/js/actions/projectSelection.js
@@ -1,38 +1,24 @@
 import { getProject } from './project';
+import { projectList as mockProjectList } from '../__mockData';
 
 export const GET_PROJECT_LIST = 'GET_PROJECT_LIST';
-
-const projectList = [
-    {
-        url: 'http://design.oweather.net/',
-        title: 'weather',
-        _id: 'wthr',
-        image: 'http://design.oweather.net/6D6CF3F4-6064-4B05-AFBB-EBFC79333CC2/artboard.png',
-    },
-    {
-        url: 'http://mokhovyk.com/raccoon/',
-        title: 'raccoon design',
-        _id: 'rccn',
-        image: 'http://mokhovyk.com/raccoon/076FEA6F-2118-4F87-AD9F-2043812E9DF9/artboard.png',
-    },
-    {
-        url: 'http://mokhovyk.com/raccoon_test/',
-        title: 'test design',
-        _id: 'tst',
-        image: 'http://mokhovyk.com/raccoon_test/47191062-5006-483D-B616-0503EC65923D/artboard.png',
-    },
-];
 
 export function getProjectList(activeProjectId = null) {
     return dispatch => {
         return fetch('', {})
             .then((response) => {
+                // mock response
+                const projectList = mockProjectList;
+
                 dispatch({
                     type: GET_PROJECT_LIST,
                     projectList,
                 });
             })
             .then((response) => {
+                // mock response
+                const projectList = mockProjectList;
+
                 if (activeProjectId) {
                     const activeProject = projectList.find(item => (
                         item._id === activeProjectId)
