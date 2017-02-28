46a4a6f59e024e592bac67a0d1c68d65b49bcdf8
Andrii Durnov <andrii_durnov@epam.com>
2017-02-28 13:14:04 +0200

    [webpack] cleanup

--- src/back/bootstrap/preMiddleware.js
+++ src/back/bootstrap/preMiddleware.js
@@ -10,4 +10,4 @@ module.exports = (app) => app
 	.use(function(req, res, next) {
 		res.header('Access-Control-Allow-Origin', '*');
 		next();
-	});;
+	});
--- src/back/controllers/user/auth.service.js
+++ src/back/controllers/user/auth.service.js
@@ -25,11 +25,6 @@ exports.getAccount = (req, res) => {
     });
 };
 
-
-
-
-
-
 exports.logout = (req, res) => {
     req.logout();
     res.sendStatus(200);
